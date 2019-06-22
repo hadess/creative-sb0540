72c757f4de74f05dc8282a6232be2bd12cd61f6a
Bastien Nocera <hadess@hadess.net>
2019-06-22 15:29:46 +0200

    Simplify copyright

--- hid-creative-sb0540.c
+++ hid-creative-sb0540.c
@@ -1,16 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * HID driver for the Creative SB0540 receiver
  *
  * Copyright (C) 2019 Red Hat Inc. All Rights Reserved
  *
- * This software is licensed under the terms of the GNU General Public
- * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  */
 
 #include <linux/device.h>
